d907476a2740d41f07cef6a3e0d820e56ab037da
Dmitry Matyshev <deemoor4@gmail.com>
2025-12-29 17:28:03 +0300

    fix: changed api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Topic } from '../types';
 
-const API_URL = 'https://api.jsonblob.com/019b60be-f547-7a84-b9de-79bb6a8106dd';
+const API_URL = 'https://api.jsonblob.com/019b6a80-e898-7117-9eb8-15dddf97c8dd';
 
 export const api = {
   getTopics: async (): Promise<Topic[]> => {
